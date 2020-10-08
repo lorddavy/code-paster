e4dc9d5c96cd64534364e4664eac6d1f4422ff96
perezpod <david.perez_povedano@roche.com>
2020-10-08 10:15:44 +0200

    Added Redux and a silly reducer

--- front-redux/src/index.tsx
+++ front-redux/src/index.tsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import { StylesProvider } from '@material-ui/styles';
 import App from './app';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducers } from './reducer';
+
+const store = createStore(reducers
+    ,window['__REDUX_DEVTOOLS_EXTENSION__'] && window['__REDUX_DEVTOOLS_EXTENSION__']()
+  );
+
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <StylesProvider injectFirst>
       <App />
     </StylesProvider>
-  </>,
+  </Provider>,
   document.getElementById('root')
 );
